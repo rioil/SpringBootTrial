2baac38c1edc44ef37552cb144c95e0544f20b8e
rioil <rioil.dev@gmail.com>
2022-08-09 16:57:16 +0900

    initial commit

---
+++ src/test/java/com/example/springboottrial/SpringBootTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
